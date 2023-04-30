3dbe59b778c32afbead72bac50efda0005c74013
Ivanchenko59 <ivanchenko59@gmail.com>
2023-04-30 20:35:54 +0300

    moved calibration function to the separate file

---
+++ software/calibration.py
@@ -0,0 +1,59 @@
+import numpy as np
+import json
+
+FILENAME = "calibration_data.json"
+
+def write_calibration_data(data):
+        with open(FILENAME, 'w') as f:
+            json.dump(data, f)
+
+def read_calibration_data():
+    with open(FILENAME, 'r') as file:
+        data = json.load(file)
+    return data
+
+def perform_calibration(measurement_data):
+    calibration_data = read_calibration_data()
+    ampl_cal_interp = np.interp(
+        measurement_data['frequency'], 
+        calibration_data['frequency'], 
+        calibration_data['calib_adc']
+    )
+    calibrated_data = np.subtract(
+        convert_adc_array_to_dBm(measurement_data['raw_adc']), 
+        convert_adc_array_to_dBm(convert_adc_array_to_mV(ampl_cal_interp))
+        )
+    return measurement_data['frequency'], calibrated_data
+
+
+def convert_adc_to_mV(data_adc):
+    data_mV = data_adc / 4095 * 3300
+    return data_mV
+
+def convert_adc_array_to_mV(data_adc_array):
+    data_mV_array = []
+    for data_adc in data_adc_array:
+        data_mV = data_adc / 4095 * 3300
+        data_mV_array.append(data_mV)
+    return data_mV_array
+
+def convert_mV_to_dBm(data_mV):
+    slope = 25          # mV / dBm
+    intercept = -84      # dBm
+    
+    data_dBm = data_mV / slope + intercept
+    return data_dBm
+
+def convert_mV_array_to_dBm(data_mV_array):
+    slope = 25          # mV / dBm
+    intercept = -84      # dBm
+    
+    data_dBm_array = []
+    for data_mV in data_mV_array:
+        data_dBm = data_mV / slope + intercept
+        data_dBm_array.append(data_dBm)
+    
+    return data_dBm_array
+
+def convert_adc_array_to_dBm(data_adc):
+    return convert_mV_array_to_dBm(convert_adc_array_to_mV(data_adc))
--- software/controller.py
+++ software/controller.py
@@ -4,16 +4,13 @@ from main_window import MainWindow
 from device import Device
 
 import serial.tools.list_ports
-import numpy as np
-import json
 import debugpy
 
+import calibration as calib
 from utils import get_smooth_func, filter_func
 
 class Controller:
     def __init__(self):
-
-        self.filename = "calibration_data.json"
 
         # gui
         self.app = QApplication([])
@@ -140,10 +137,10 @@ class Controller:
                 data['calib_adc'].append(adc_data)
 
             # data['calib_adc'] = get_smooth_func(data['calib_adc'])
-            data['calib_adc'] = filter_func(data['calib_adc'])
+            # data['calib_adc'] = filter_func(data['calib_adc'])
 
             # Save data to JSON file
-            self.write_calibration_data(data)
+            calib.write_calibration_data(data)
 
             self.progress_dialog.close()
             return True
@@ -154,60 +151,9 @@ class Controller:
     def analyzer_stop(self):
         self.continuous_acquisition = False
 
-    def write_calibration_data(self, data):
-        with open(self.filename, 'w') as f:
-            json.dump(data, f)
-
-    def read_calibration_data(self):
-        with open(self.filename, 'r') as file:
-            data = json.load(file)
-        return data
-
-    def perform_calibration(self, measurement_data):
-        calibration_data = self.read_calibration_data()
-        ampl_cal_interp = np.interp(
-            measurement_data['frequency'], 
-            calibration_data['frequency'], 
-            calibration_data['calib_adc']
-        )
-        calibrated_data = np.subtract(
-            self.convert_mV_array_to_dBm(self.convert_adc_array_to_mV(measurement_data['raw_adc'])), 
-            self.convert_mV_array_to_dBm(self.convert_adc_array_to_mV(ampl_cal_interp))
-            )
-        return measurement_data['frequency'], calibrated_data
-
-    def convert_adc_to_mV(self, data_adc):
-        data_mV = data_adc / 4095 * 3300
-        return data_mV
-
-    def convert_adc_array_to_mV(self, data_adc_array):
-        data_mV_array = []
-        for data_adc in data_adc_array:
-            data_mV = data_adc / 4095 * 3300
-            data_mV_array.append(data_mV)
-        return data_mV_array
-
-    def convert_mV_to_dBm(self, data_mV):
-        slope = 25          # mV / dBm
-        intercept = -84      # dBm
-        
-        data_dBm = data_mV / slope + intercept
-        return data_dBm
-    
-    def convert_mV_array_to_dBm(self, data_mV_array):
-        slope = 25          # mV / dBm
-        intercept = -84      # dBm
-        
-        data_dBm_array = []
-        for data_mV in data_mV_array:
-            data_dBm = data_mV / slope + intercept
-            data_dBm_array.append(data_dBm)
-        
-        return data_dBm_array
-
 
     def data_ready_callback(self):
-        self.calibrated_data = self.perform_calibration(self.acquisition_worker.raw_data)
+        self.calibrated_data = calib.perform_calibration(self.acquisition_worker.raw_data)
         
         self.main_window.screen.plot_ch(
             self.calibrated_data[0], self.calibrated_data[1]
@@ -217,7 +163,7 @@ class Controller:
         #     self.acquisition_worker.raw_data['frequency'], self.acquisition_worker.raw_data['raw_adc']
         # )
         self.main_window.screen.plot_ch(
-            self.acquisition_worker.raw_data['frequency'], self.convert_mV_array_to_dBm(self.convert_adc_array_to_mV(self.acquisition_worker.raw_data['raw_adc']))
+            self.acquisition_worker.raw_data['frequency'], calib.convert_adc_array_to_dBm(self.acquisition_worker.raw_data['raw_adc'])
         )
 
         if self.continuous_acquisition == True:
